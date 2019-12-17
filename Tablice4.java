593d9fee9b414805c25bb4bb3f413d8576b6c279
gporlowski <58628126+gporlowski@users.noreply.github.com>
2019-12-17 19:51:38 +0100

    Tablice 4

--- Tablice4.java
+++ Tablice4.java
@@ -1,4 +1,4 @@
-public class Tablice1 {
+public class Tablice4 {
     public static void main(String[] args) {
         int[] array = new int[]{7, 4, 15, 9, 17, 3, 22, 6, 122, 8};
         System.out.println("Suma wszystkich elementów tablicy wynosi: " + getSum(array));
