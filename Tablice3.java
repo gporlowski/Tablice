4119b5f74c817ec1b9343eae0013c68ab9b43b68
gporlowski <58628126+gporlowski@users.noreply.github.com>
2019-12-17 19:50:16 +0100

    Tablice 3

--- Tablice3.java
+++ Tablice3.java
@@ -1,7 +1,7 @@
 public class Tablice3 {
     public static void main(String[] args) {
         int[] array = new int[]{7, 4, 15, 9, 17, 3, 22, 6, 122, 8};
-        System.out.println("Największa liczba w tablicy to: " + getMin(array));
+        System.out.println("Najmniejsza liczba w tablicy to: " + getMin(array));
     }
     public static int getMin (int[] arrayOfNumbers) {
         int minValue = arrayOfNumbers[0];
